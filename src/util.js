1fa407db65c6294e4b418f9c93160519f777bfa3
CasualCloud <davidstiles62@yahoo.com>
2024-09-25 17:08:27 -0400

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,3 +1,3 @@
 export function getPcs() {
-	return game.actors.filter(actor => actor.system.type === "character" || actor.system.type === "player").map(actor =>{return {id: actor.id, name: actor.system.name, image: actor.system.img}});	
+	return game.actors.filter(actor => actor.type === "character" || actor.type === "player").map(actor =>{return {id: actor.id, name: actor.name, image: actor.img}});	
 }
