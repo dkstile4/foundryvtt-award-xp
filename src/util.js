acade6ab2952fb82042da5f8cde6b374429dcbec
dkstile4 <davidstiles62@yahoo.com>
2026-06-21 16:05:05 -0400

    ugh AI

--- src/util.js
+++ src/util.js
@@ -2,3 +2,4 @@ export function getPcs() {
 	return game.actors
 		.filter(actor => actor.hasPlayerOwner || actor.type === "character" || actor.type === "player")
 		.map(actor => ({ id: actor.id, name: actor.name, image: actor.img }))
+}
